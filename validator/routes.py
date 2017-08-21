e5c3c842fae20c830b13b9265456c8ce2d6d5677
Alexander Pashuk <alexander.pashuk@xpansa.com>
2017-08-21 15:24:08 +0300

    experiments with ajax

--- validator/routes.py
+++ validator/routes.py
@@ -30,13 +30,71 @@ def upload_file():
             list_type = 'file'
             urls = request.files['file']
             if urls.filename == '':
-                return render_template('upload.html', error="Please paste some URLs/DOIs or attach .csv/.txt file")
+                return jsonify({'error': 'Please paste some URLs/DOIs or attach .csv/.txt file'}), \
+                    200, {'ContentType': 'application/json'}
+                # return render_template('upload.html', error="Please paste some URLs/DOIs or attach .csv/.txt file")
         else:
-            return render_template('upload.html', error="Please paste some URLs/DOIs or attach .csv/.txt file")
+            return jsonify({'error': 'Please paste some URLs/DOIs or attach .csv/.txt file'}), \
+                200, {'ContentType': 'application/json'}
+            # return render_template('upload.html', error="Please paste some URLs/DOIs or attach .csv/.txt file")
         # Step 2. Add processing in queue
         task_id = Task.create_task(list_type, urls)
-        return redirect(url_for('app_routes.render_results', task_id=task_id))
+        return jsonify({'task_id': task_id, 'error': ''}), \
+            200, {'ContentType': 'application/json'}
+        # return redirect(url_for('app_routes.render_results', task_id=task_id))
     return render_template('upload.html')
+
+
+@app_routes.route('/check-results', methods=['POST'])
+def check_results():
+    """
+        Check results by task ID
+    """
+    if request.form['task_id']:
+        task_id = request.form['task_id']
+        papers_list = List.objects(task_id=task_id).first()
+        if not papers_list:
+            return jsonify({'error': 'No list of papers for this task ID'}), \
+                200, {'ContentType': 'application/json'}
+
+        # if still processing return logs
+        if not papers_list.is_processed:
+            logs = Log.objects(list_id=papers_list)
+            results = {'is_processed': False, 'logs':[]}
+            for log in logs:
+                results['logs'].append({
+                    'id': log.get_id(),
+                    'date_created': log.date_created,
+                    'message': log.message
+                })
+            return jsonify({'results': results}), 200, {'ContentType': 'application/json'}
+        # if processed return results
+        papers = Paper.objects(list_id=papers_list.get_id())
+        results = {
+            'is_processed': papers_list.is_processed,
+            "papers": []
+        }
+        for paper in papers:
+            notebooks = Notebook.objects(paper_id=paper.get_id())
+            tmp_list = []
+            for notebook in notebooks:
+                tmp_list.append(
+                    {
+                        "id": notebook.get_id(),
+                        "filename": notebook.filename,
+                        "url": notebook.original_url,
+                        "is_failed": notebook.is_failed,
+                        "message": re.sub(r'(\\n)+', '\n', notebook.message).replace('\\n', '<br>')
+                    }
+                )
+            results["papers"].append({
+                "paper_url": paper.original_url,
+                "notebooks": tmp_list,
+                "url_type": paper.url_type
+            })
+        return jsonify({'results': results}), 200, {'ContentType': 'application/json'}
+    return jsonify({'error': 'No task ID specified in query'}), \
+        200, {'ContentType': 'application/json'}
 
 
 @app_routes.route('/results/<task_id>', methods=['GET'])
--- validator/static/js/main.js
+++ validator/static/js/main.js
@@ -1,7 +1,65 @@
 $(function () {
+    var timerId = null;
+
     console.log('Ready to go!');
+    // update placeholder for file input
     $(document).on('change', '.custom-file-input', function () {
         console.log('Changed value of file input');
-        $(this).parent().find('.custom-file-control').attr('data-content', $(this).val().replace(/C:\\fakepath\\/i, '').substring(0, 40));
+        $(this).parent().find('.custom-file-control').attr(
+            'data-content',
+            $(this).val().replace(/C:\\fakepath\\/i, '').substring(0, 40)
+        );
     });
+
+    // upload form button listener
+    $('.main-form button').click(function (e) {
+        e.preventDefault();
+        var fd = new FormData($('.main-form')[0]);
+
+        $.ajax({
+            url: '/upload',
+            data: fd,
+            processData: false,
+            contentType: false,
+            type: 'POST',
+            success: function (data) {
+                if (data['error']) {
+                    $('.form-control-feedback').html(data['error']);
+                    return;
+                }
+                var task_id = data.task_id;
+                var container = $('body .container .row');
+                container.html('');
+
+                // create timer
+                timerId = setInterval(checkStatus, 3000, task_id);
+            }
+        });
+
+        function checkStatus(task_id) {
+            $.ajax({
+                url: '/check-status',
+                data: {task_id: task_id},
+                type: 'POST',
+                success: function(data) {
+                    console.log(data);
+                    if (data.is_processed) {
+                        console.log('IS PROCESSED');
+                        clearInterval(timerId);
+                    } else {
+                        console.log('IS NOT PROCESSED');
+                        updateLog(data.results);
+                    }
+                }
+            })
+        }
+
+        function updateLog(messages) {
+            var container = $('body .container .row');
+            for (var i = 0; i < messages.length; i++) {
+                container.append('<p>' + messages[i]['message'] + '</p>');
+            }
+        }
+
+    })
 });
